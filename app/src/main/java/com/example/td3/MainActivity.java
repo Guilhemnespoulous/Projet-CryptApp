9d44d97f7fb4054c7df4304549e4520744dadaae
Guilhemnespoulous <nespoulous@et.esiea.fr>
2020-05-10 01:44:25 +0200

    Ajout branche API rest

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        
+
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
